7992a4ea4c7a750c66a9427399e1b7b248be871e
swoops503 <james.roundy4@gmail.com>
2023-04-19 16:38:29 -0700

    added some pseudocode to greater-than-five.js

--- greater-than-5.js
+++ greater-than-5.js
@@ -3,7 +3,7 @@
 let num1 = 5;    //variables to test function of if/else statement
 let num2 = 6;
 
-if (num1 >= 5 && num2 >=5) {
+if (num1 >= 5 && num2 >=5) { // testing if both numbers 1 & 2 are greater than or equal to 5
     console.log("true")
 } else{
     console.log("false")
